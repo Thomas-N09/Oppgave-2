11df33cb9cd9da177f2f55ed37cf8466dc4eb940
Thomas-N09 <nyth092024@gmail.com>
2024-10-07 12:28:54 +0200

    Oppgave 2

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Oppgave_2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int newInt = 20;
+        Console.WriteLine(newInt);
+        string newString = "Hellow World!";
+        Console.WriteLine(newString);
+        double newDouble = 10.5;
+        Console.WriteLine(newDouble);
+        char newChar = '3';
+        Console.WriteLine(newChar);
+        int[] newArray = {1, 2, 3, 4, 5};
+        
+        for (int i = 0; i < newArray.Count(); i++)
+        {
+            Console.WriteLine(newArray[i]);
+        }
+    }
+}
